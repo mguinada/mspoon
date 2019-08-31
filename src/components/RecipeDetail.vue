7fab7dc6e94a032a75df2f8142d4715154edaa53
Miguel Guinada <mguinada@gmail.com>
2019-08-31 18:48:00 +0100

    Add the `RecipeDetail` component

---
+++ src/components/RecipeDetail.vue
@@ -0,0 +1,45 @@
+<template lang='html'>
+  <div class='recipe-detail'>
+    <p class='title'>{{ title }}</p>
+    <div class='thumb'>
+      <img :src='image'>
+    </div>
+    <ul class='tags'>
+      <li class='tag' v-for='tag in tags' :key='tag'>{{ tag }}</li>
+    </ul>
+    <p class='description'>{{ description }}</p>
+    <p class='chef'>{{ chef }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'recipeDetail',
+  props: {
+    recipe: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    title () {
+      return this.recipe.fields.title
+    },
+    image () {
+      return this.recipe.fields.photo.fields.file.url
+    },
+    description () {
+      return this.recipe.fields.description
+    },
+    chef () {
+      return this.recipe.fields.chef.fields.name
+    },
+    tags () {
+      return this.recipe.fields.tags.map((t) => { return t.fields.name })
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ tests/unit/components/RecipeDetail.spec.js
@@ -0,0 +1,58 @@
+import { mount } from "@vue/test-utils"
+import RecipeDetail from '@/components/RecipeDetail.vue'
+
+describe('Recipe.vue', () => {
+  let component
+
+  beforeEach(async () => {
+    component = mount(
+      RecipeDetail,
+      {
+        propsData: {
+          recipe: {
+            sys: {
+              id: '1'
+            },
+            fields: {
+              title: 'Bacon & eggs',
+              description: 'Lorem ipsum dolor sit amet...',
+              chef: {
+                fields: {
+                  name: 'Jay Oliver'
+                }
+              },
+              tags: [
+                {
+                  fields: {
+                    name: 'Breakfast'
+                  }
+                },
+                {
+                  fields: {
+                    name: 'Fast meal'
+                  }
+                }
+              ],
+              photo: {
+                fields: {
+                  file: {
+                    url: 'https://cdn.example.org/bacon-and-eggs.png'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    )
+  })
+
+  it('Renders the recipe detail', () => {
+    expect(component.html()).toContain(`<p class=\"title\">Bacon &amp; eggs</p>`)
+    expect(component.html()).toContain(`<p class=\"description\">Lorem ipsum dolor sit amet...</p>`);
+    expect(component.html()).toContain(`<li class=\"tag\">Breakfast</li>`)
+    expect(component.html()).toContain(`<li class=\"tag\">Fast meal</li>`)
+    expect(component.html()).toContain(`<p class=\"chef\">Jay Oliver</p>`)
+    expect(component.html()).toContain(`<img src="https://cdn.example.org/bacon-and-eggs.png">`);
+  });
+});
